141656b3f9268377581a1750b613658035531e37
Alexander Forselius <drsounds@gmail.com>
2023-10-21 20:08:14 +0200

    Initial Commit

---
+++ Assignment4/Assignment4App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment4App.swift
+//  Assignment4
+//
+//  Created by admin on 2023-10-21.
+//
+
+import SwiftUI
+
+@main
+struct Assignment4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
